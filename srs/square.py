6dbdb2c574828e463da5b2406c19afb95c9aba4e
Zuraze Ali <zuraze.ali@gmail.com>
2021-01-12 17:11:05 -0500

    Class Square

--- srs/square.py
+++ srs/square.py
@@ -3,8 +3,8 @@ class Square:
         self.location = location
         self.piece = piece
 
-    def location(self, location):
+    def coordinates(self, location):
         location = {'x': int, 'y':int}
 
-    def piece(self, piece):
+    def chess_piece(self, piece):
         pass
