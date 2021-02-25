d5accec2f2389116aebf22e0afde675c98a7e3fb
Zuraze Ali <zuraze.ali@gmail.com>
2021-02-25 17:54:43 -0500

    Test_cases Rook and Queen

--- src/test_suite/tests.py
+++ src/test_suite/tests.py
@@ -19,4 +19,3 @@ moves = pawn_moves + bishop_moves + rook_moves + knight_moves + queen_moves + ki
     #'en passant white':['C2 C4', 'H7 H6','C4 C5', 'D7 D5', 'C5 D6'],
     #'en passant black':['A2 A3', 'C7 C5', 'A3 A4', 'C5 C4', 'D2 D4', 'C4 D3'],
     #'bishop capture':['D2 D4', 'A7 A5', 'C1 D2', 'B7 B6', 'D2 E3', 'C7 C6', 'E3 F4', 'G7 G5', 'F4 G5'],
-
