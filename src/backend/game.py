cdae053838ac59e9181971a51e8245b33582cdac
Wasif Khan <wasif.k1112@gmail.com>
2021-04-25 01:51:09 -0400

    Create game.py

---
+++ src/backend/game.py
@@ -0,0 +1,11 @@
+'''
+Class to simulate a game
+'''
+
+from board import Board
+
+class Game:
+  def __init__(self, p1_name='Player 1', p2_name='Player 2'):
+    self.player_1 = p1_name
+    self.player_2 = p2_name
+    self.board = Board()
