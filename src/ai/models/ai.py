322cd8a10ddd7aab5d534e4733f908439b91464f
WasifKhan <wasif.k1112@gmail.com>
2021-04-26 00:25:21 -0400

    Add ai code

--- src/ai/models/ai.py
+++ src/ai/models/ai.py
@@ -5,6 +5,15 @@ Abstract Base Class for AI Engines
 import abc
 
 class AI(metaclass=ABCMeta):
-    def __init__(self):
-        pass
+    def __init__(self, board):
+        self.board = board
 
+    @classmethod
+    def board_to_datapoint(self):
+        return board
+
+    def make_move(self):
+        raise NotImplemented
+
+    def get_move(self, move):
+        raise NotImplemented
