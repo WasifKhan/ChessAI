fdfd331ce1fdce3b9902aa1ed5d64020ef196f77
WasifKhan <wasif.k1112@gmail.com>
2021-04-26 19:37:18 -0400

    Add raw data

--- src/ai/data/data_extractor.py
+++ src/ai/data/data_extractor.py
@@ -1,3 +1,37 @@
 '''
-File to extract data from the web
+File to extract chess games data from the web
 '''
+
+import bz2
+from urllib.request import urlopen
+from files import files
+
+
+class ExtractData:
+    def __init__(self, files):
+        self.files = files
+        self.files_processed = 0
+        self.filename = 'raw_data/data'
+        self.chunk = 16 * 1024
+
+    def extract_file(self, file, ID):
+        decompressor = bz2.BZ2Decompressor()
+        filename = self.filename + str(ID) + '.txt'
+        try:
+            with open(filename, 'wb') as fp:
+                req = urlopen(file)
+                while chunk := req.read(self.chunk):
+                    decomp = decompressor.decompress(chunk)
+                    if decomp:
+                        fp.write(decomp)
+        except EOFError:
+            fp.close()
+            return
+
+    def extract_data(self):
+        for ID in range(len(self.files)):
+            self.extract_file(self.files[ID], ID)
+
+
+data_extractor = ExtractData(files)
+data_extractor.extract_data()
