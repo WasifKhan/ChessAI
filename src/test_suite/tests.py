ac5d0d2f60bf954243c3973b702de7ba6f9ed55d
Wasif Khan <wasif.k1112@gmail.com>
2021-04-27 03:57:05 -0400

    Update tests.py

--- src/test_suite/tests.py
+++ src/test_suite/tests.py
@@ -12,10 +12,10 @@ from .pieces_test_cases.king_tests import moves as king_moves
 # list of the moves for the game
 # c = list
 # list of Pieces (where you expect them to be after)
-
+# IE.
+# ('bishop upper right diagonal white movement',
+#        ['D2 D4', 'D7 D5', 'C1 E3', 'C8 E6'],
+#        [Bishop(is_white=True, location=(4,2)),
+#         Bishop(is_white=False, location=(4, 5))])
 
 moves = pawn_moves + bishop_moves + rook_moves + knight_moves + queen_moves + king_moves
-    #'vertical_single':['A2 A3', 'D7 D6', 'B2 B3', 'E7 E6'],
-    #'en passant white':['C2 C4', 'H7 H6','C4 C5', 'D7 D5', 'C5 D6'],
-    #'en passant black':['A2 A3', 'C7 C5', 'A3 A4', 'C5 C4', 'D2 D4', 'C4 D3'],
-    #'bishop capture':['D2 D4', 'A7 A5', 'C1 D2', 'B7 B6', 'D2 E3', 'C7 C6', 'E3 F4', 'G7 G5', 'F4 G5'],
