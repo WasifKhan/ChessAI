6090c22c8734bae3673b91c93fe3b61148ac9561
WasifKhan <wasif.k1112@gmail.com>
2021-04-27 01:54:37 -0400

    merge data extractor/converter

--- src/ai/data/data_extractor.py
+++ src/ai/data/data_extractor.py
@@ -5,8 +5,11 @@ File to extract chess games data from the web
 import bz2
 from urllib.request import urlopen
 from files import files as FILES
+import os
+import re
 
 LOCATION = 'raw_data/'
+DATASET_LOCATION = 'dataset/data.txt'
 
 class ExtractData:
     def __init__(self, location, files):
@@ -33,6 +36,45 @@ class ExtractData:
         for ID in range(len(self.files)):
             self.extract_file(self.files[ID], ID)
 
+class DataConverter:
+    def __init__(self, source, destination):
+        self.source = source
+        self.destination = destination
+        self.data = []
+
+    def extract_moves(self, lines):
+        for line in lines[:-1]:
+            if line[0] == '1':
+                game = re.split('[\d]+\.\s', line)
+                datapoint = '['
+                for it in range (1, len(game)):
+                    move = game[it].split('{')
+                    if len(move) > 1:
+                        white_move = re.split('\W', move[0][0:-1])[0]
+                        black_move = re.split('\W', move[-2].split()[-1])[0]
+                    else:
+                        white_move = re.split('\W', move[0].split()[0])[0]
+                        black_move = re.split('\W', move[0].split()[1])[0]
+                    if it + 1 == len(game) and white_move == black_move:
+                        black_move = 'None'
+                    datapoint += f'({white_move}, {black_move}), '
+                datapoint = datapoint[0:-2] + ']\n'
+                self.data.append(datapoint)
+
+    def raw_data_to_dataset(self):
+        for filename in os.listdir(self.source):
+            with open(self.source + filename) as fp:
+                lines = fp.readlines()
+                self.extract_moves(lines)
+        with open(self.destination, 'w') as fp:
+            for datapoint in self.data:
+                fp.write(datapoint)
+
+
+'''
+converter = DataConverter(LOCATION, DATASET_LOCATION)
+converter.raw_data_to_dataset()
 
 data_extractor = ExtractData(LOCATION, FILES)
 data_extractor.extract_data()
+'''
