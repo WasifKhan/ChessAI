4d36f1e4080d3442f12539e82a93eeaac2bd0d36
Ali-k1 <60724675+Ali-k1@users.noreply.github.com>
2021-05-04 01:48:57 -0400

    change board coordinate system

--- tests/unit_tests/test_bishop.py
+++ tests/unit_tests/test_bishop.py
@@ -7,7 +7,7 @@ from src.backend.board import Board
 
 class TestBishop:
     @fixture
-    def wite_bishop(self):
+    def white_bishop(self):
         return Bishop(is_white=True, location=(0,2))
     
     @fixture
@@ -15,13 +15,42 @@ class TestBishop:
         return Bishop(is_white=False, location=(7,2))
 
     def test_init(self, black_bishop, white_bishop):
-        return True
+        assert white_bishop.is_white == True
+        assert white_bishop.location[0] == 0
+        assert white_bishop.location[1] == 2
+        assert black_bishop.is_white == False
+        assert black_bishop.location[0] == 7
+        assert black_bishop.location[1] == 2
 
-    def test_str(self):
-        return True
+    def test_str(self, white_bishop, black_bishop):
+        assert str(white_bishop) == 'B'
+        assert str(black_bishop) == 'b'
 
-    def test_is_valid_move(self):
-        return True
+    def test_is_valid_move(self, black_bishop, white_bishop):
+        board = Board()
+        assert white_bishop.is_valid_move(board, (1,3)) == False
+        assert white_bishop.is_valid_move(board, (1,0)) == False
+        board[3,3] = white_bishop
+        white_bishop.location=(3,3)
+        assert white_bishop.is_valid_move(board, (4,4)) == True
+        assert white_bishop.is_valid_move(board, (5,5)) == True
+        assert white_bishop.is_valid_move(board, (6,6)) == True
+        assert white_bishop.is_valid_move(board, (4,2)) == True
+        assert white_bishop.is_valid_move(board, (6,0)) == True
+        assert white_bishop.is_valid_move(board, (4,3)) == False
+        assert white_bishop.is_valid_move(board, (2,3)) == False
+        assert white_bishop.is_valid_move(board, (2,2)) == True
+        assert white_bishop.is_valid_move(board, (1,1)) == False
+        board[3,4] = black_bishop
+        black_bishop.location=(3,4)
+        assert black_bishop.is_valid_move(board, (1,3)) == False
+        assert black_bishop.is_valid_move(board, (2,3)) == True
+        assert black_bishop.is_valid_move(board, (1,2)) == True
+        assert black_bishop.is_valid_move(board, (1,6)) == True
+        assert black_bishop.is_valid_move(board, (5,6)) == True
+        assert black_bishop.is_valid_move(board, (5,2)) == True
+
+        
 
     def test_moves(self):
         return True
