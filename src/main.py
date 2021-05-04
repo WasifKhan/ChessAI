8cbebc10e4903f82e786e5f75e04d2f2a178cf6a
WasifKhan <wasif.k1112@gmail.com>
2021-05-04 19:25:36 -0400

    Rename game.py to model.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 if __name__ == '__main__':
     from interface import Interface
     from frontend.view import View
-    from backend.game import Game
+    from backend.model import Game
 
     View(Interface(Game()))
