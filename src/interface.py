bc85a939dbb57ebe16a2924fcc3459add4b16f0d
WasifKhan <wasif.k1112@gmail.com>
2021-05-08 18:19:29 -0400

    simplify interface

--- src/interface.py
+++ src/interface.py
@@ -2,6 +2,7 @@
 Interface between backend and frontend
 '''
 
+from time import sleep
 
 class Interface:
     def __init__(self, game, ai):
@@ -49,49 +50,22 @@ class Interface:
         print('Invalid move.\nPlease enter a valid move.\n')
         return False
 
-    def _sim_many(self):
-        self.game.board.__init__()
-        self.set_player_names('White AI', 'Black AI')
-        white_turn = True
-        while not self.game.is_game_over():
-            move = self.ai_move(white_turn)
-            if move is None:
-                break
-            white_turn = not(white_turn)
-        return not(self.game.white_turn)
-
-    def _sim_game(self):
-        print((' ' * 8 + '\n') * 50)
-        for i in range(2):
-            print('Preparing game...')
-        self.game.board.__init__()
-        print(self)
-        self.set_player_names('White AI', 'Black AI')
-        white_turn = True
-        while not self.game.is_game_over():
-            player = 'White AI' if white_turn else 'Black AI'
-            for i in range(2):
-                print(f'{player} is thinking...')
-            move = self.ai_move(white_turn)
-            if move is None:
-                break
-            white_turn = not(white_turn)
-            if white_turn:
-                print(self)
-        return not(self.game.white_turn)
-
-    def simulate_games(self, num_games: int=0):
-        if not num_games:
-            white_win = self._sim_game()
-            return white_win
+    def simulate_games(self, num_games: int):
         white_wins, black_wins = 0,0
         for i in range(num_games):
-            if self._sim_many():
+            self.game.__init__('White AI', 'Black AI')
+            white_turn = True
+            while not self.game.is_game_over():
+                if (move := self.ai_move(white_turn)) is None:
+                    break
+                white_turn = not(white_turn)
+            if not(self.game.white_turn):
                 white_wins += 1
+                print('White wins')
             else:
                 black_wins += 1
-        print(white_wins)
-        print(black_wins)
+                print('Black wins')
+        print(f'score: {white_wins}-{black_wins}')
 
     def get_scoreboard(self):
         return True
