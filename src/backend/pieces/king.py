0699c901b081139c23f6374e372fc1fe79bcc13a
Ali-k1 <60724675+Ali-k1@users.noreply.github.com>
2021-05-11 03:41:49 -0400

    king is_valid complete, partial king move

--- src/backend/pieces/king.py
+++ src/backend/pieces/king.py
@@ -1,3 +1,4 @@
+from io import DEFAULT_BUFFER_SIZE
 from .piece import Piece
 from .pawn import Pawn
 
@@ -23,7 +24,7 @@ class King(Piece):
         elif self.is_white is board[destination].is_white:
             return False
         else:
-            for piece in board.pieces:
+            for piece in (pieces := board.black_pieces if self.is_white else board.white_pieces):
                 #Edge case for pawn check
                 if isinstance(piece, Pawn):
                     direction = 1 if piece.is_white else -1
@@ -34,7 +35,19 @@ class King(Piece):
         return True
 
     def moves(self, board):
-        return set()
+        result = set()
+        possible_location = {piece.location[0]*10 + piece.location[1] \
+            for row in range(-1, 2) for col in range(-1, 2) if (row or col) and (piece := board[self.location[0]+row*10, self.location[1]+col])}
+        opposing_attacks = set()
+        for piece in (pieces := board.black_pieces if self.is_white else board.white_pieces):
+            if ...: # need to implement edge case for pawn
+                ...
+            else:
+                opposing_attacks.add(piece.move(board))
+        result = possible_location - opposing_attacks
+        return result
+
+
 
     def checkmate(self, board):
         return False
