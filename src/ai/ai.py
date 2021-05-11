6be3d6b9e5e3c186d42d4e3361e23206a438fec3
Wasif <wasif@gmail.com>
2021-05-11 03:49:47 -0400

    Clean bugs in data_extractor

--- src/ai/ai.py
+++ src/ai/ai.py
@@ -3,15 +3,11 @@ Main AI Engine
 '''
 
 from .names import MODELS
-from .data.data_extrator import data_extractor
 
 
 
 class AI:
     def __init__(self, difficulty=0, name='AI'):
-        # The following line take approximately 15 hours to run to train AI
-        data_extractor.raw_data_to_dataset()
-
         ai_file, ai_cls = MODELS[difficulty]
         location = f'./ai/models/{ai_file}'
         exec(f'from .models.{ai_file}.{ai_file} import {ai_cls} as ai')
